6b0ed6ca3d1fd5c5d7c0f82d27464ea3cbc5b46b
Daniel Proctor <dan@danielproctor.dev>
2024-11-20 21:48:17 +0000

    modified:   entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ if [ -f /packages.txt ]; then
     cat /packages.txt | xargs apk add
 fi
 
-RUN echo "${CRON_INTERVAL} /cron/run.sh >> /proc/1/fd/1 2>/proc/1/fd/2" > cronconfig
+echo "${CRON_INTERVAL} /cron/run.sh >> /proc/1/fd/1 2>/proc/1/fd/2" > cronconfig
 
 chmod 0644 /cron/cronconfig
 crontab /cron/cronconfig
